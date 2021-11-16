2c05e87a83082d073c4c2affbf68eafea36e01ea
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:41:40 +0000

    Add props to OrderList.cs

--- CoffeSoftware/Coffe.Domain/Models/OrderList.cs
+++ CoffeSoftware/Coffe.Domain/Models/OrderList.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Coffe.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coffe.Domain.Models
 {
-    class OrderList
+    class OrderList : Entity
     {
+        public string Name { get; set; }
+        public DateTime CreationTime { get; set; }
     }
 }
