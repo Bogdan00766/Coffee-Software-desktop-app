4224a4096a526079434e4827fd87841cf3e87d9b
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:53:33 +0000

    Add DbSets

--- CoffeSoftware/CoffeSoftware.Infrastructure/CoffeDbContext.cs
+++ CoffeSoftware/CoffeSoftware.Infrastructure/CoffeDbContext.cs
@@ -8,6 +8,11 @@ namespace Coffe.Infrastructure
 {
     class CoffeDbContext : DbContext
     {
-         
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<OrderList> OrderList { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Shop> Shop { get; set; }
+        public DbSet<User> User { get; set; }
     }
 }
