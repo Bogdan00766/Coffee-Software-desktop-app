d36674be6b2a9acfaaafa8288210a3d4a0f3c4f8
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:38:04 +0000

    Add props to User.cs

--- CoffeSoftware/Coffe.Domain/Models/User.cs
+++ CoffeSoftware/Coffe.Domain/Models/User.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Coffe.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coffe.Domain.Models
 {
-    class User
+    class User : Entity
     {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
