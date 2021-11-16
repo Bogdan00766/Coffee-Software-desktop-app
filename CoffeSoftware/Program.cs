def40ad8cb4f20cd6254c496bd26b8278dbbe7b1
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:18:48 +0000

    Dodaj pliki projektów.

---
+++ CoffeSoftware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
