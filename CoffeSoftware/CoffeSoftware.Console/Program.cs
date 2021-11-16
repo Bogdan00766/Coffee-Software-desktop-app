fde191ce4a38fa9b3c9b3c10dee1f83c8b08f08e
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:27:47 +0000

    Created Project and CoffeSoftware.Console

---
+++ CoffeSoftware/CoffeSoftware.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeSoftware.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
