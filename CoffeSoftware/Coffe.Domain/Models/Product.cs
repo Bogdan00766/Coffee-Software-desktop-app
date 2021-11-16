523273d1ad363fd7db1ed2c7138f5c584a265783
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:42:48 +0000

    Add props to Product.cs

--- CoffeSoftware/Coffe.Domain/Models/Product.cs
+++ CoffeSoftware/Coffe.Domain/Models/Product.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Coffe.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coffe.Domain.Models
 {
-    class Product
+    class Product : Entity
     {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImagePath { get; set; }
     }
 }
