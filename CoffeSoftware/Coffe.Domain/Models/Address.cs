00904d11451ea5a857cb3154bccd4e2b9dbc35a1
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-11-16 13:40:18 +0000

    Add props to Address.cs

--- CoffeSoftware/Coffe.Domain/Models/Address.cs
+++ CoffeSoftware/Coffe.Domain/Models/Address.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Coffe.Domain.SeedWork;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coffe.Domain.Models
 {
-    class Address
+    class Address : Entity
     {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+        public string Country { get; set; }
     }
 }
