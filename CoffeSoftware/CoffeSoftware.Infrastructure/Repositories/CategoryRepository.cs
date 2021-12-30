0a96fdce3db47f784855e401522e511482d7c5cf
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 13:29:00 +0100

    Created Category Repository

---
+++ CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Coffe.Domain.Models;
+using Coffe.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffe.Infrastructure.Repositories
+{
+    class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(CoffeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
