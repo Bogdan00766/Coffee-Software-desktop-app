3206059513a1d33f34be01ca8a44db62c27c26ef
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 13:29:54 +0100

    Created OrderListRepository

---
+++ CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/OrderListRepository.cs
@@ -0,0 +1,16 @@
+﻿using Coffe.Domain.Models;
+using Coffe.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffe.Infrastructure.Repositories
+{
+    class OrderListRepository : Repository<OrderList>, IOrderListRepository
+    {
+        public OrderListRepository(CoffeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
