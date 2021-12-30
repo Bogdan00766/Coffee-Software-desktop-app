860b0bea72097470e3a011e721e32133155c6a3a
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 13:27:14 +0100

    Created AddressRepository

--- CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/AddressRepository.cs
+++ CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/AddressRepository.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Coffe.Domain.Models;
+using Coffe.Domain.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Coffe.Infrastructure.Repositories
 {
-    class AddressRepository
+    class AddressRepository : Repository<Address>, IAddressRepository
     {
+        public AddressRepository(CoffeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
     }
 }
