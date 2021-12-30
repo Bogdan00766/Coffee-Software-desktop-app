cc023b24a31ef10ab1846d57e2d1bb81f84f275e
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 14:14:37 +0100

    Created ShopRepository

---
+++ CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/ShopRepository.cs
@@ -0,0 +1,16 @@
+﻿using Coffe.Domain.Models;
+using Coffe.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffe.Infrastructure.Repositories
+{
+    class ShopRepository : Repository<Shop>, IShopRepository
+    {
+        public ShopRepository(CoffeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
