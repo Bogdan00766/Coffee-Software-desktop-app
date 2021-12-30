13ad820f74c5b53c5dce81cee9a67b64f5cf2cfc
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 14:26:34 +0100

    Created variables for Repositories in UoW

---
+++ CoffeSoftware/CoffeSoftware.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Coffe.Domain.Models;
+using Coffe.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coffe.Infrastructure.Repositories
+{
+    class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(CoffeDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
--- CoffeSoftware/CoffeSoftware.Infrastructure/UnitOfWork.cs
+++ CoffeSoftware/CoffeSoftware.Infrastructure/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using Coffe.Domain;
 using Coffe.Domain.Repositories;
+using Coffe.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -11,8 +12,12 @@ namespace Coffe.Infrastructure
     public class UnitOfWork : IUnitOfWork
     {
         private CoffeDbContext _dbContext;
-        //public ICategoryRepository CategoryRepository => new CategoryRepository(_dbContext);
-        //public IProductRepository ProductRepository => new ProductRepository(_dbContext);
+        public IAddressRepository AddressRepository => new AddressRepository(_dbContext);
+        public ICategoryRepository CategoryRepository => new CategoryRepository(_dbContext);
+        public IOrderListRepository OrderListRepository => new OrderListRepository(_dbContext);
+        public IProductRepository ProductRepository => new ProductRepository(_dbContext);
+        public IShopRepository ShopRepository => new ShopRepository(_dbContext);
+        public IUserRepository UserRepository => new UserRepository(_dbContext);
 
         public UnitOfWork()
         {
