e3edc49e5c27c4305a78a6e3002a20da607104c9
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2021-12-30 13:04:41 +0100

    Created IRepository interface

---
+++ CoffeSoftware/Coffe.Domain/SeedWork/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.Domain.SeedWork
+{
+    public interface IRepository<T> where T : Entity
+    {
+        T Create(T e);
+        T Update(T e);
+        T Delete(T e);
+        Task<T> FindByIdAsync(int id);
+        Task<List<T>> FindAllAsync();
+    }
+}
