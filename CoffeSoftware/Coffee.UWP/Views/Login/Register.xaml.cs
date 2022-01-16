14b4e7390179a79aabe5d5edd667ac50d11e41d5
Piotr Chudzik-Lipka <m307824@alunos.ipb.pt>
2022-01-16 17:03:46 +0000

    Fixed popup

--- CoffeSoftware/Coffee.UWP/Views/Login/Register.xaml.cs
+++ CoffeSoftware/Coffee.UWP/Views/Login/Register.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Coffe.Domain;
+using Coffe.Domain.Models;
+using Coffe.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -27,70 +30,55 @@ namespace Coffee.Uwp.Views.Login
             this.InitializeComponent();
         }
 
-        private void registerButton_Click(object sender, RoutedEventArgs e)
+        private async void registerButton_Click(object sender, RoutedEventArgs e)
         {
-            if(emailText.Text == String.Empty)
-            {
-                infoText.Text = "Email cannot be empty!"; 
-            }
+            if (emailText.Text == String.Empty) infoText.Text = "Email cannot be empty!";
+            else if (usernameText.Text == String.Empty) infoText.Text = "Username cannot be empty!";
+            else if (passwordText.Text == String.Empty) infoText.Text = "Password cannot be empty!";
+            else if (streetText.Text == String.Empty) infoText.Text = "Street cannot be empty!";
+            else if (cityText.Text == String.Empty) infoText.Text = "City cannot be empty!";
+            else if (postalCodeText.Text == String.Empty) infoText.Text = "Postal code cannot be empty!";
+            else if (!int.TryParse(postalCodeText.Text, out _)) infoText.Text = "Postal code has to be a number!";
+            else if (!emailText.Text.Contains('@')) infoText.Text = "Wrong e-mail addres!";
+            else if (passwordText.Text.Length < 5) infoText.Text = "Password is to short (at least 5 characters)";
             else
             {
-                if (usernameText.Text == String.Empty)
+                IUnitOfWork uow = new UnitOfWork();
+                if ((await uow.UserRepository.isRegisteredAsync(emailText.Text)))
                 {
-                    infoText.Text = "Username cannot be empty!";
+                    infoText.Text = "Email address already in use";
                 }
                 else
                 {
-                    if (passwordText.Text == String.Empty)
+                    User user = new User();
+                    user.IsAdmin = false;
+                    user.Email = emailText.Text;
+                    user.Username = usernameText.Text;
+                    user.Password = passwordText.Text;
+
+                    Address address = new Address();
+                    address.Street = streetText.Text;
+                    address.City = cityText.Text;
+                    address.PostalCode = int.Parse(postalCodeText.Text);
+                    if (countryText.Text != string.Empty) address.Country = countryText.Text;
+                    if (stateText.Text != string.Empty) address.State = stateText.Text;
+
+                    address = uow.AddressRepository.Create(address);
+                    user.Address = address;
+                    uow.UserRepository.Create(user);
+                    await uow.SaveAsync();
+
+
+                    ContentDialog RegisterSuccessDialog = new ContentDialog()
                     {
-                        infoText.Text = "Password cannot be empty!";
-                    }
-                    else
-                    {
-                        if (streetText.Text == String.Empty)
-                        {
-                            infoText.Text = "Street cannot be empty!";
-                        }
-                        else
-                        {
-                            if (cityText.Text == String.Empty)
-                            {
-                                infoText.Text = "City cannot be empty!";
-                            }
-                            else
-                            {
-                                if (postalCodeText.Text == String.Empty)
-                                {
-                                    infoText.Text = "Postal code cannot be empty!";
-                                }
-                                else
-                                {
-                                    if (!int.TryParse(postalCodeText.Text, out _))
-                                    {
-                                        infoText.Text = "Postal code has to be a number!";
-                                    }
-                                    else
-                                    {
-                                        if (!emailText.Text.Contains('@'))
-                                        {
-                                            infoText.Text = "Wrong e-mail addres!";
-                                        }
-                                        else
-                                        {
-                                            if (passwordText.Text.Length<5)
-                                            {
-                                                infoText.Text = "Password is to short (at least 5 characters)";
-                                            }
-                                            else
-                                            {
-                                                //
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                        Title = "Register Success!",
+                        Content = "You are registered now! Please log in",
+                        CloseButtonText = "Ok!"
+                    };
+
+                    this.Frame.Navigate(typeof(Login));
+                    await RegisterSuccessDialog.ShowAsync();
+                    
                 }
             }
         }
