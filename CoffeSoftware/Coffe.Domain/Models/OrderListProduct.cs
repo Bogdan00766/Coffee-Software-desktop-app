f8d27ce63c8a6f6efa7c8890987fab5267ad3379
Diana Savchuk <m307835@alunos.ipb.pt>
2022-01-18 13:50:04 +0200

    Create constructor for OrderListProduct model

--- CoffeSoftware/Coffe.Domain/Models/OrderListProduct.cs
+++ CoffeSoftware/Coffe.Domain/Models/OrderListProduct.cs
@@ -11,5 +11,22 @@ namespace Coffe.Domain.Models
         public OrderList OrderList { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
+
+        public OrderListProduct()
+        {
+
+        }
+
+        public OrderListProduct(
+            int orderListId,
+            OrderList orderList, 
+            int productId, 
+            Product product)
+        {
+            OrderListId = orderListId;
+            OrderList = orderList;
+            ProductId = productId;
+            Product = product;
+        }
     }
 }
