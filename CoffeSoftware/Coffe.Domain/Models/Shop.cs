2394eb75df1e3b488a7220fbe7a72a1e93a1bc1a
Diana Savchuk <m307835@alunos.ipb.pt>
2022-01-18 13:52:22 +0200

    Create constructor for Shop model

--- CoffeSoftware/Coffe.Domain/Models/Shop.cs
+++ CoffeSoftware/Coffe.Domain/Models/Shop.cs
@@ -8,8 +8,24 @@ namespace Coffe.Domain.Models
     public class Shop : Entity
     {
         public string Name { get; set; }
-        public string ImagePath { get; set; }
+        public byte[] ImagePath { get; set; }
         public float Price { get; set; }
         public List<Product> Products { get; set; }
+
+        public Shop()
+        {
+
+        }
+
+        public Shop(string name, 
+            byte[] imagePath, 
+            float price, 
+            List<Product> products)
+        {
+            Name = name;
+            ImagePath = imagePath;
+            Price = price;
+            Products = products;
+        }
     }
 }
